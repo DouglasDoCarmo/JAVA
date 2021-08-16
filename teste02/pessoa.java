7e0a8f036389032a126bc78548baad9ab6fced89
Douglas do Carmo <87789671+DouglasDoCarmo@users.noreply.github.com>
2021-08-16 16:10:25 -0300

    update

--- teste02/pessoa.java
+++ teste02/pessoa.java
@@ -5,7 +5,7 @@ public class pessoa {
     public String cpf;
     public String endereco;
     public String RG;
-    public String data_nascimento;
+    public Date data_nascimento;
 
     public pessoa(String_nome, String_cpf, String_endereco, String_RG,string_data){
         this.nome= _nome;

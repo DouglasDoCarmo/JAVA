beb48dce96437373d72051575fc6fd2a19c6f490
Douglas do Carmo <87789671+DouglasDoCarmo@users.noreply.github.com>
2021-08-16 15:42:17 -0300

    Classes e objetos

---
+++ teste/src/pessoa.java
@@ -0,0 +1,17 @@
+import java.util.Date;
+
+public class pessoa {   
+    public String nome;
+    public String cpf;
+    public String endereco;
+    public String RG;
+    public String data_nascimento;
+
+    public pessoa(String_nome, String_cpf, String_endereco, String_RG,string_data){
+        this.nome= _nome;
+        this.cpf= _cpf;
+        this.endereco= RG;
+        this.data_nascimento= _data;
+    }
+    
+}

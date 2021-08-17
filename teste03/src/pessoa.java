462d9f0feea3d1d7791d6f776eab9dd1f039c40a
Douglas do Carmo <87789671+DouglasDoCarmo@users.noreply.github.com>
2021-08-17 14:28:02 -0300

    Encapsulamento

---
+++ teste03/src/pessoa.java
@@ -0,0 +1,27 @@
+package src;
+
+import java.util.Date;
+
+public class pessoa {   
+    protected String nome;
+    protected String cpf;
+    public String endereco;
+    protected String RG;
+    public Date data_nascimento;
+    
+    public pessoa(String _nome, String _cpf, String _endereco, String _RG, Date _data){
+        this.nome = _nome;
+        this.cpf = _cpf;
+        this.endereco = _endereco;
+        this.RG = _RG;
+        this.data_nascimento =_data;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+}    

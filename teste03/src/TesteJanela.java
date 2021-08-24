508034c765b4e712871786c25495b080c225e4e1
Douglas do Carmo <87789671+DouglasDoCarmo@users.noreply.github.com>
2021-08-24 16:16:50 -0300

    Interface

--- teste03/src/TesteJanela.java
+++ teste03/src/TesteJanela.java
@@ -1,3 +1,10 @@
+import javax.swing.*;
 public class TesteJanela {
-    
+//Objeto Janela
+public static void main(String args[] ){
+    final JFrame janela = JFrame("Título ");
+    janela.setBounds(50, 100, 400, 150);// Seta posição e tamanho
+    janela.setDefaultCloseOperation( WindowConstants.DISPOSE_ON_CLOSE);
+    janela.setVisible(true); // Exibe a janela
+}   
 }

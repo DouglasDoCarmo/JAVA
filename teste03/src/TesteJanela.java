2623db0ab31006f365f902823c42df1757f88f6c
Douglas do Carmo <87789671+DouglasDoCarmo@users.noreply.github.com>
2021-08-24 19:56:32 -0300

    Update TesteJanela.java

--- teste03/src/TesteJanela.java
+++ teste03/src/TesteJanela.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 public class TesteJanela {
 //Objeto Janela
 public static void main(String args[] ){
-    final JFrame janela = JFrame("Título ");
+    final JFrame janela = JFrame("Título da janela - Curso FIC");
     janela.setBounds(50, 100, 400, 150);// Seta posição e tamanho
     janela.setDefaultCloseOperation( WindowConstants.DISPOSE_ON_CLOSE);
     janela.setVisible(true); // Exibe a janela
